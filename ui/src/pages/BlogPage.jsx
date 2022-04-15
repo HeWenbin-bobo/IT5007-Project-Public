a3df90c847f218cd8a4e2ee3d77a964305beb21c
HeWenbin-bobo <hewenbin243854232@outlook.com>
2022-04-15 17:00:57 +0800

    First commit

--- ui/src/pages/BlogPage.jsx
+++ ui/src/pages/BlogPage.jsx
@@ -2,7 +2,6 @@ import { Link as RouterLink } from 'react-router-dom';
 // material
 import { Grid, Button, Container, Stack, Typography } from '@mui/material';
 // components
-import Page from '../components/Page';
 import Iconify from '../components/Iconify';
 import BlogPostCard from '../components/Blog/BlogPostCard';
 import BlogPostsSort from '../components/Blog/BlogPostsSort';
@@ -22,28 +21,26 @@ const SORT_OPTIONS = [
 
 export default function BlogPage() {
     return (
-        <Page title="Dashboard: Blog">
-            <Container>
-                <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
-                    <Typography variant="h4" gutterBottom>
-                        Blog
-                    </Typography>
-                    <Button variant="contained" /*component={RouterLink}*/ to="#" startIcon={<Iconify icon="eva:plus-fill" />}>
-                        New Post
-                    </Button>
-                </Stack>
+        <Container>
+            <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
+                <Typography variant="h4" gutterBottom>
+                    Blog
+                </Typography>
+                <Button variant="contained" /*component={RouterLink}*/ to="#" startIcon={<Iconify icon="eva:plus-fill" />}>
+                    New Post
+                </Button>
+            </Stack>
 
-                <Stack mb={5} direction="row" alignItems="center" justifyContent="space-between">
-                    <BlogPostsSearch posts={POSTS} />
-                    <BlogPostsSort options={SORT_OPTIONS} />
-                </Stack>
+            <Stack mb={5} direction="row" alignItems="center" justifyContent="space-between">
+                <BlogPostsSearch posts={POSTS} />
+                <BlogPostsSort options={SORT_OPTIONS} />
+            </Stack>
 
-                <Grid container spacing={3}>
-                    {POSTS.map((post, index) => (
-                        <BlogPostCard key={post.id} post={post} index={index} />
-                    ))}
-                </Grid>
-            </Container>
-        </Page>
+            <Grid container spacing={3}>
+                {POSTS.map((post, index) => (
+                    <BlogPostCard key={post.id} post={post} index={index} />
+                ))}
+            </Grid>
+        </Container>
     );
 }

b46447b64078d53d87923d91558dbf0577816f0c
HeWenbin-bobo <hewenbin243854232@outlook.com>
2022-04-16 19:44:09 +0800

    First commit

--- api/rabbitmq.js
+++ api/rabbitmq.js
@@ -6,9 +6,9 @@ async function sendMessage() {
             var q = 'hello';
             var msg = 'Hello World!';
 
-            var ok = ch.assertQueue(q, {durable: false});
+            var ok = await ch.assertQueue(q, {durable: false});
 
-            await ok.then(async function(_qok) {
+            if (ok) {
                 // NB: `sentToQueue` and `publish` both return a boolean
                 // indicating whether it's OK to send again straight away, or
                 // (when `false`) that you should wait for the event `'drain'`
@@ -16,26 +16,28 @@ async function sendMessage() {
                 // so we'll ignore it.
                 ch.sendToQueue(q, Buffer.from(msg));
                 console.log(" [x] Sent '%s'", msg);
-                await ch.close();
-                return true;
-            });
-        }).finally(async function() { await conn.close(); });
+                return ch.close();
+            };
+        }).finally(function() { conn.close(); });
     }).catch(console.warn);
 }
 
 async function receiveMessage() {
-    amqp.connect('amqp://localhost').then(function(conn) {
+    await amqp.connect('amqp://localhost').then(function(conn) {
+        process.once('SIGINT', function() { conn.close(); });
         return conn.createChannel().then(async function(ch) {
+
             var ok = ch.assertQueue('hello', {durable: false});
 
-            await ok.then(async function(_qok) {
-                ch.consume('hello', function(msg) {
+            if (ok) {
+                await ch.consume('hello', function(msg) {
                     console.log(" [x] Received '%s'", msg.content.toString());
                 }, {noAck: true});
-                await ch.close();
-                return true;
-            });
-        }).finally(async function() { await conn.close(); });
+            };
+
+            return ch.close();
+
+        }).finally(function() { conn.close(); });
     }).catch(console.warn);
 }
 

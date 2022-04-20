27c1422916783249058ac6c5a090faffbdf059c8
HeWenbin-bobo <hewenbin243854232@outlook.com>
2022-04-20 21:02:34 +0800

    First commit

--- ui/src/pages/AssetsPage.jsx
+++ ui/src/pages/AssetsPage.jsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
+// import Box from '@mui/material/Box';
+// import Toolbar from '@mui/material/Toolbar';
+// import Container from '@mui/material/Container';
+// import Grid from '@mui/material/Grid';
+// import Paper from '@mui/material/Paper';
 // import Chart from '../components/Chart';
 // import Balance from '../components/Balance';
 // import Topup from '../components/Topup';
@@ -13,6 +13,11 @@ import Paper from '@mui/material/Paper';
 // import WatchList from '../components/WatchList/WatchList';
 
 import loadable from '@loadable/component';
+const Box = loadable(() => import('@mui/material/Box'));
+const Toolbar = loadable(() => import('@mui/material/Toolbar'));
+const Container = loadable(() => import('@mui/material/Container'));
+const Grid = loadable(() => import('@mui/material/Grid'));
+const Paper = loadable(() => import('@mui/material/Paper'));
 const Chart = loadable(() => import('../components/Chart'));
 const Balance = loadable(() => import('../components/Balance'));
 const Topup = loadable(() => import('../components/Topup'));

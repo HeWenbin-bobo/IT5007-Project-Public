eac9bf863a6cac972fdb35bbd1885657d418199c
HeWenbin-bobo <hewenbin243854232@outlook.com>
2022-04-20 19:55:15 +0800

    First commit

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
-import HomePage from './pages/HomePage';
+import loadable from '@loadable/component';
+const HomePage = loadable(() => import('./pages/HomePage'));
+// import HomePage from './pages/HomePage';
 import {HashRouter as Router} from 'react-router-dom';
 
 
